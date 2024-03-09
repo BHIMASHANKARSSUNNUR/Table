757c64245e9924d5fc347692907aadf11a602c19
BHIMASHANKARSSUNNUR <102892417+BHIMASHANKARSSUNNUR@users.noreply.github.com>
2024-03-09 17:53:14 +0530

    Table

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'table';
+
+  student=[
+    {id:1,name:"angular"},
+    {id:2,name:"react"},
+    {id:3,name:"vue"},
+    {id:4,name:"bootstrap"},
+    {id:5,name:"foundation"}
+
+  ]
+  Searchtext=""
+  
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './search.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
